b33939bf08df23cc670b357d87e3adb2ecf5167a
parkcrystal <9jeworld@naver.com>
2020-06-21 14:01:51 +0900

    채팅 목록 화면 출현

--- app/src/main/java/com/phis/numberbaseballgame/MainActivity.kt
+++ app/src/main/java/com/phis/numberbaseballgame/MainActivity.kt
@@ -2,8 +2,14 @@ package com.phis.numberbaseballgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.phis.numberbaseballgame.datas.Chat
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.numberbaseballgame_20200621.adapters.ChatAdapter
 
 class MainActivity : BaseActivity() {
+
+    val chatList = ArrayList<Chat>()
+    lateinit var mChatAdapter: ChatAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        chatList.add(Chat("CPU", "숫자 야구게임에 오신 것을 환영합니다."))
+        chatList.add(Chat("CPU", "세 자리 숫자를 맞춰주세요."))
+        chatList.add(Chat("CPU", "0은 포함되지 않으며, 중복된 숫자도 없습니다."))
+
+        mChatAdapter = ChatAdapter(mContext, R.layout.chat_list_item, chatList)
+        chatListView.adapter = mChatAdapter
 
     }
 
